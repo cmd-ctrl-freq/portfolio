f64f7e7e2a65fbb0a22544c615c809b86c30df0a
David Fentz <bomjumaku@gmail.com>
2018-12-28 16:53:41 -0500

    timeline resizes for mobile devices. The code looks wonky but it's working.

--- src/components/Experience.vue
+++ src/components/Experience.vue
@@ -3,22 +3,42 @@
       <v-container>
         Ideally, I'll add some text here explaining what this section is!
       </v-container>
-      <v-timeline class="dense-sm-and-down">
-    <v-timeline-item
-      v-for="event in History"
-      :key="event.description"
-      color="red lighten-2"
-      large
-    >
-      <span slot="opposite">{{event.time}}</span>
-      <v-card class="elevation-2">
-        <v-card-title class="headline">{{event.description}}</v-card-title>
-        <v-card-text>
-          {{event.details}}
-        </v-card-text>
-      </v-card>
-    </v-timeline-item>
-  </v-timeline>
+      <v-container  class="hidden-sm-and-down">
+        <v-timeline>
+          <v-timeline-item
+            v-for="event in History"
+            :key="event.description"
+            color="red lighten-2"
+            large
+          >
+            <span slot="opposite">{{event.time}}</span>
+            <v-card class="elevation-2">
+              <v-card-title class="headline">{{event.description}}</v-card-title>
+              <v-card-text>
+                {{event.details}}
+              </v-card-text>
+            </v-card>
+          </v-timeline-item>
+        </v-timeline>
+      </v-container>
+      <v-container  class="hidden-md-and-up">
+        <v-timeline dense>
+          <v-timeline-item
+            v-for="event in History"
+            :key="event.description"
+            color="red lighten-2"
+            large
+          >
+            <span slot="opposite">{{event.time}}</span>
+            <v-card class="elevation-2">
+              <v-card-title class="headline">{{event.description}}</v-card-title>
+              <v-card-text>
+                {{event.details}}
+              </v-card-text>
+            </v-card>
+          </v-timeline-item>
+        </v-timeline>
+      </v-container>
     </v-container>
 </template>
 

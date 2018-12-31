5c7230b874b6a2436d80547b86e700c7d3dd7832
David Fentz <bomjumaku@gmail.com>
2018-12-31 12:22:45 -0500

    Finished the RF replay write-up.

--- src/components/individual_projects/RFReplayProject.vue
+++ src/components/individual_projects/RFReplayProject.vue
@@ -73,7 +73,46 @@
       <v-flex d-flex xs12 sm6 md8>
           <v-flex id="Section1">
               <p>
-               This will say something.
+               Once you've identified the rough frequency range that your device is operating
+                on, and you've set up a SDR to listen around that range, you can actually 
+                start listening to, or watching (figure 1) it! What you see in that image is
+                the RF packet that the Fob is sending. This, in and of itself, is a
+                lot of fun. 
+              </p>
+              <p>
+                However, this information is not sufficient to manipulate all systems. For 
+                older cars you can actually just record this packet, replay it over a tansmitter,
+                and enjoy all the privelages of that Fob indefinitely. For newer systems, or to
+                reverse engineer the communication protocols being used, it's probably best to
+                take a look at the actual message. Luckily, that's pretty easy too.
+              </p>
+          </v-flex>
+      </v-flex>
+      <v-flex d-flex xs12 sm12 md12>
+          <v-flex id="Section1">
+              <p>
+               SDR# is the program that I used to listen to the Fob signal originally. It's free and
+               with a lot of very cheap recieving antennas out of the box. This program can capture 
+               data from the reciever and record it as a .wav file. The great thing about this is 
+               we can just open it up in <a href="https://www.audacityteam.org/">Audacity</a> 
+               and investigate/manipulate the packet manually. 
+              </p>
+              <p>
+                You can see the detailed signal in figure 2. This signal is using 
+                <a href="https://en.wikipedia.org/wiki/Amplitude-shift_keying">Amplitude Shift Keying</a>.
+                The binary bits are, examining the signal visually, "short pulse" and "long pulse". It doesn't 
+                matter which is '0' and which is '1', only that they are distinct. 
+              </p>
+              <p> 
+                You can see that there is this span of short pulses at the beginning of the packet (figure 2 is
+                actually showing three instances of the same packet sent at different times) which is suspiciously 
+                long. This is for synchronization of the Fob transmitter and the vehicle's reciever. It's impossible
+                to determing where the data starts without comparing 2 or more unique packets with eachother. 
+              </p>
+              <p>
+                The last step of this attack on my vehicle (which does not use
+                 <a href="https://en.wikipedia.org/wiki/Rolling_code">rolling codes</a>), is to replay the
+                 packets from the Fob. This is what's shown in the video at the top of the write-up.
               </p>
           </v-flex>
       </v-flex>
